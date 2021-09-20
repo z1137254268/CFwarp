ad8037050d5cb7e6b992172a299244c602a28ff3
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-09-20 13:39:39 +0800

    Update and rename 8.sh to 9.sh

--- 9.sh
+++ 9.sh
@@ -158,6 +158,16 @@ echo $modify | sh
 	# 把 wgcf-profile.conf 复制到/etc/wireguard/ 并命名为 wgcf.conf
 	cp -f wgcf-profile.conf /etc/wireguard/wgcf.conf
 
+wg-quick up wgcf >/dev/null 2>&1
+	v4=$(wget -T1 -t1 -qO- -4 ip.gs)
+	v6=$(wget -T1 -t1 -qO- -6 ip.gs)
+	until [[ -n $v4 && -n $v6 ]]
+	  do
+	   wg-quick down wgcf >/dev/null 2>&1
+	   wg-quick up wgcf >/dev/null 2>&1
+	   v4=$(wget -T1 -t1 -qO- -4 ip.gs)
+	   v6=$(wget -T1 -t1 -qO- -6 ip.gs)
+	done
 	
 	systemctl enable wg-quick@wgcf >/dev/null 2>&1
 	
