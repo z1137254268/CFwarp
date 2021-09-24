3dd7676632c3b9ee8f2544f2322e1bb274d6c3ac
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-09-24 14:01:03 +0800

    Update and rename beta9.sh to beta10.sh

--- beta10.sh
+++ beta10.sh
@@ -266,6 +266,9 @@ lsmod | grep bbr
 function cwarp(){
 systemctl stop wg-quick@wgcf
 systemctl disable wg-quick@wgcf
+if [[ ${vi} == " lxc" ]]; then
+echo -e "search blue.kundencontroller.de\noptions rotate\nnameserver 2a02:180:6:5::1c\nnameserver 2a02:180:6:5::4\nnameserver 2a02:180:6:5::1e\nnameserver 2a02:180:6:5::1d" > /etc/resolv.conf
+fi
 sudo reboot
 }
 
