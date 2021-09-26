a1b17cb223d5b18ae51c48d7c91036b28579e032
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-09-27 07:26:17 +0800

    Update and rename 3.sh to 4.sh

--- 4.sh
+++ 4.sh
@@ -81,7 +81,9 @@ on)
 WARPIPv4Status=$(green "WARP已开启，当前IPV4地址：$v4 ，IP区域简称：$gj4 ") 
 ;; 
 off) 
-WARPIPv4Status=eval \$(yellow "WARP未开启，当前IPV4地址：$v4 ，IP区域简称： $gj4 ") 
+WARPIPv4Status=$(yellow "WARP未开启 ") 
+WARPIPv4Status=$(yellow "WARP未开启，当前IPV4地址：$v4 ") 
+WARPIPv4Status=yellow "eval echo \$$gj4 ") 
 esac 
 else
 WARPIPv4Status=$(red "不存在IPV4地址 ")
