d5fe8dfce59cd0c499afdd561d3dd5bfa56cb4c3
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-10-07 21:21:42 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,16 @@
 
 wg-quick down wgcf
 systemctl restart wg-quick@wgcf
-wg-quick up wgcf >/dev/null 2>&1
+wg-quick up wgcf
 v4=$(wget -T1 -t1 -qO- -4 ip.gs)
 v6=$(wget -T1 -t1 -qO- -6 ip.gs)
 until [[ -n $v4 || -n $v6 ]]
 do
-wg-quick down wgcf >/dev/null 2>&1
-wg-quick up wgcf >/dev/null 2>&1
+wg-quick down wgcf
+wg-quick up wgcf
 v4=$(wget -T1 -t1 -qO- -4 ip.gs)
 v6=$(wget -T1 -t1 -qO- -6 ip.gs)
 done
 
-$v4
-$v6
+echo $v4
+echo $v6
