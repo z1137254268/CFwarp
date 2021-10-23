84d868a63811eab97439d0a223202277635bc6a5
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-10-23 13:03:21 +0800

    Update and rename un1.sh to un2.sh

--- un2.sh
+++ un2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-white(){
-    echo -e "\033[1;37m\033[01m$1\033[0m"
+bblue(){
+    echo -e "\033[1;34m\033[01m$1\033[0m"
 }
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
@@ -10,7 +10,7 @@ yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-white " lxc/openvz小鸡TUN是否启用反馈如下" 
+bblue "lxc/openvz小鸡TUN是否启用反馈如下" 
 $(cat /dev/net/tun) 
 green "注意：如上反馈内容末尾显示 << in bad state >>，说明已启用TUN，支持安装WARP，恭喜！"
 yellow "注意：如上反馈内容末尾显示 << not permitted >>，说明未启用TUN，不支持安装WARP，请联系VPS厂商开通！"
